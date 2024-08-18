83fe0523fc3e462c9fcb9ba2496b4b42e75fbcd5
Zaid Dagbanbia <mohammed.dagbanbia@dci-student.org>
2024-08-18 16:35:53 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import Navbar from "./component/Navbar/Navbar"
+import Sidebar from "./component/Sidebar/Sidebar"
+import {Routes , Route} from 'react-router-dom'
+import Add from "./pages/Add/Add"
+import List from "./pages/List/List"
+import Orders from "./pages/Orders/Orders"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const App = () => {
+
+  const url = "http://localhost:9005"
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+
+        <Sidebar />
+        <Routes>
+          
+          <Route  path="/add"   element={<Add url={url}/>} />
+          <Route  path="/list"   element={<List url={url}/>} />
+          <Route  path="/orders"   element={<Orders url={url}/>} />
+        </Routes>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
